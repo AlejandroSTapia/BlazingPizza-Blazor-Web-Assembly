58fd69321cd1a780a4fce677502269c06c6ef38c
Alejandro Tapia <alejandro_tapia@autotodo.com>
2024-05-31 12:35:30 -0600

    Add DTO - Entities
    Se creo el DTO(entidad que se va a compartir entre el back y front) que son objetos planos o estructuras simples

--- NorthWind.BlazingPizza.Entities/Dtos/GetSpecials/PizzaSpecialDto.cs
+++ NorthWind.BlazingPizza.Entities/Dtos/GetSpecials/PizzaSpecialDto.cs
@@ -3,7 +3,7 @@
 	public class PizzaSpecialDto(int id, string name, 
 		decimal basePrice, string description, string imageUrl)
 	{
-		public int Id => id;//solo expondra el parametro id hacia el exterior, no se mostrara en pantalla pero es identificador
+		public int Id => id;  //solo expondra el parametro id hacia el exterior, no se mostrara en pantalla pero es identificador
 		public string Name => name;
 		public decimal BasePrice => basePrice;
 		public string Description => description;
