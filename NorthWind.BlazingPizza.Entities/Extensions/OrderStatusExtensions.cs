82f6abc7e4a8156d15b246208dc473b60fc35cb8
Alejandro Tapia <alejandro_tapia@autotodo.com>
2024-06-28 12:40:22 -0600

    changes adelanto

---
+++ NorthWind.BlazingPizza.Entities/Extensions/OrderStatusExtensions.cs
@@ -0,0 +1,22 @@
+﻿using NorthWind.BlazingPizza.Entities.Enums;
+using NorthWind.BlazingPizza.Entities.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.BlazingPizza.Entities.Extensions
+{
+    public static class OrderStatusExtensions
+    {
+        public static string ToFriendlyString(this OrderStatus status) =>
+            status switch
+            {
+                OrderStatus.Preparing => Messages.PreparingText,
+                OrderStatus.OutForDelivery => Messages.OutForDeliveryText,
+                OrderStatus.Delivered => Messages.DeliveredText,
+                _ => Messages.UnknownStatusText
+            };
+    }
+}
---
+++ NorthWind.BlazingPizza.Frontend.WebApiProxies/Orders/OrdersModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.BlazingPizza.Frontend.WebApiProxies.Orders
+{
+    internal class OrdersModel
+    {
+    }
+}
