634a80bb85c71c0e1889e13199df8889fb480907
Alejandro Tapia <alejandro_tapia@autotodo.com>
2024-09-10 20:18:05 -0600

    Fluentvalidation - Implementar nuevo framework para construir las reglas de validacion con la bibloteca Fluentvalidation
    Se agrego una nueva biblioteca de clases afuera de todo(pero dentro de la carpeta 4) para agregar el framework(si es uno porque se tiene codigo que puede utilizar el cliente) a diferencia de una biblioteca.
    
    se agrego una clase AbstractValidationImplementation donde se hara un wrapper o implementation ya que Fluentvalidation ya brinda esta clase pero se hara el wrapper

---
+++ NorthWind.ValidationService.FluentValidation/AbstractValidationImplementation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.ValidationService.FluentValidation
+{
+    //tiene los metodos para agregar las reglas pero es abstracta, porlo qu eno se puede instanciar y se requiere heredarla
+    internal class AbstractValidationImplementation
+    {
+    }
+}
